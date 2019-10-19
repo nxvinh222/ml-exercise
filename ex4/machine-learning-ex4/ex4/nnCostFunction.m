786ecb92838afe410abcff0d7765d5c0d1fad404
unknown <nxvinh222@gmail.com>
2019-10-19 20:01:41 +0700

    upload

--- ex4/machine-learning-ex4/ex4/nnCostFunction.m
+++ ex4/machine-learning-ex4/ex4/nnCostFunction.m
@@ -106,11 +106,13 @@ end
 Theta1_grad = (1/m) * Theta1_grad
 Theta2_grad = (1/m) * Theta2_grad
 
+%add regulization to cost function
+J = J + (lambda/(2*m)) * (sum(sum(Theta1(:, 2:end).^2)) + sum(sum(Theta2(:, 2:end).^2)))
 
+%add regulization to gradient
 
-
-
-
+Theta1_grad = Theta1_grad + (lambda/m) * [zeros(size(Theta1,1),1) Theta1(:, 2:end)]
+Theta2_grad = Theta2_grad + (lambda/m) * [zeros(size(Theta2,1),1) Theta2(:, 2:end)]
 
 
 
